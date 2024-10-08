cc86b716f5587db696a41b4874a604d6a41ec4b1
k.castro <k.castro@mefasa.org>
2024-10-08 15:18:59 -0700

    metodos en livewire

---
+++ resources/views/livewire/hijo.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <x-button class="mb-4" wire:click='$parent.increment'>
+        Incrementar
+    </x-button>
+
+</div>
--- resources/views/livewire/paises.blade.php
+++ resources/views/livewire/paises.blade.php
@@ -1,16 +1,28 @@
 <div>
 
+    <x-button class="mb-2" wire:click="$set('count',0)">
+        Mostrar / Ocultar
+    </x-button>
+
     <form class="mb-4" wire:submit='save'>
-        <x-input wire:model='pais' placeholder="Ingrese un pais" />
+        <x-input wire:model='pais' placeholder="Ingrese un pais" wire:keydown.space='increment' />
         <x-button>
             Agregar
         </x-button>
     </form>
 
     <ul class="list-disc list-inside space-y-2">
-        @foreach($paises as $pais)
-        <li> {{ $pais }} <x-danger-button>X</x-danger-button> </li>
+        @foreach($paises as $index => $pais)
+        <li wire:key='pais-{{$index}}'>
+            <span wire:mouseenter='changeActive("{{ $pais }}")'>
+                ({{ $index }}) {{ $pais }}
+            </span>
+        <x-danger-button wire:click='delete({{ $index }})'>
+            X
+        </x-danger-button> </li>
 
         @endforeach
     </ul>
+    {{ $active}}
+    {{ $count }}
 </div>
