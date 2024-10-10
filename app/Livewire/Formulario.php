a1ee1910d5c6dcaab3ffebfeba74e74efa9a150f
k.castro <k.castro@mefasa.org>
2024-10-10 07:35:04 -0700

    creacion de componentes select y text area para formulario

--- app/Livewire/Formulario.php
+++ app/Livewire/Formulario.php
@@ -3,9 +3,34 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\Tag;
 
 class Formulario extends Component
 {
+
+    public $categories, $tags;
+
+    public $category_id, $title, $content;
+
+    public $selected_tags = [];
+
+    public function mount()
+    {
+        $this->categories = Category::all();
+        $this->tags = Tag::all();
+    }
+
+    public function save()
+    {
+        dd([
+            'category_id' => $this->category_id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'tags' => $this->selected_tags
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.formulario');
--- resources/views/livewire/formulario.blade.php
+++ resources/views/livewire/formulario.blade.php
@@ -1,9 +1,53 @@
 <div>
     <div class="bg-white shadow rounded-lg p-6">
-        <form>
+        <form wire:submit='save'>
+            <div class="mb-4">
+                <label>
+                    Nombre
+                </label>
+                <x-input class="w-full" wire:model='title' required />
+            </div>
+
+            <div class="mb-4">
+                <label>
+                    Contenido
+                </label>
+                <x-text-area class="w-full" wire:model='content' required></x-text-area>
+            </div>
+            <div class="mb-4">
+                <x-label>
+                    Categoria
+                </x-label>
+                <x-select class="w-full" wire:model='category_id'>
+                    @foreach ($categories as $category )
+                        <option value="{{ $category->id }}">
+                            {{ $category->name }}
+                        </option>
+                    @endforeach
+                </x-select>
+            </div>
+
             <div>
-                <label>Nombre</label>
-                <x-input class="w-full" />
+                <x-label>
+                    Etiquetas
+                </x-label>
+                <ul>
+                    @foreach ($tags as $tag)
+                        <li>
+                            <label>
+                                <x-checkbox wire:model='selected_tags' value="{{ $tag->id }}" />
+                                {{ $tag->name }}
+                            </label>
+                        </li>
+                    @endforeach
+
+                </ul>
+            </div>
+
+            <div class="flex justify-end">
+                <x-button>
+                    Crear
+                </x-button>
             </div>
 
         </form>
